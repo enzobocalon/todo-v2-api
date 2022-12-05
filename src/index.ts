4c266dd95f4e3cd3505d9de54071af903ff44646
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-05 12:01:16 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ mongoose.connect(process.env.MONGODB_URI as string)
     const app = express();
 
     app.use(cors());
+    app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      next();
+    });
     app.use(express.json());
     app.use(router);
 
