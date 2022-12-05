ce5b3e91e8b32debd4965e98a000a229b0cd3b57
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-05 12:03:41 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ mongoose.connect(process.env.MONGODB_URI as string)
   .then(() => {
     const app = express();
 
-    app.use(cors());
+    // app.use(cors());
     app.use(function(req, res, next) {
       res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
