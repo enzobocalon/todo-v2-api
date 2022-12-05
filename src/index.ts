0868190821a248eb1a4bf7406330f25059d21f3a
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-05 12:09:45 -0300

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,17 @@ mongoose.connect(process.env.MONGODB_URI as string)
     const app = express();
 
     // app.use(cors());
-    app.use(function(req, res, next) {
-      res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      next();
+    app.use((req, res, next) => {
+      const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin as string)) {
+        res.setHeader('Access-Control-Allow-Origin', origin as string);
+      }
+      //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+      res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', true);
+      return next();
     });
     app.use(express.json());
     app.use(router);
